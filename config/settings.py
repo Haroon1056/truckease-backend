425eefdcaaa97b605240c451342f4969b688dc95
Haroon Rasheed <haroonrasheed1056@gmail.com>
2026-03-25 23:19:45 -0700

    Update settings.py file

--- config/settings.py
+++ config/settings.py
@@ -180,10 +180,15 @@ CHANNEL_LAYERS = {
 
 # Security settings for production only (SAME AS YOUR WORKING PROJECT)
 if not DEBUG:
+    ALLOWED_HOSTS.append(".onrender.com")
+
+    CSRF_TRUSTED_ORIGINS = [
+        "https://*.onrender.com",
+    ]
+
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    USE_X_FORWARDED_HOST = True
+
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
